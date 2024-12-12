b952ef36ee32406ee14934f9fc50b65ff33da295
sbx_user1051 <sbx_user1051@169.254.9.138>
2024-12-12 18:04:07 +0000

    Thu Dec 12 18:04:07 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,13 +3,18 @@ import pytest
 from lib.solutions.CHK.checkout_solution import checkout
 
 
-@pytest.mark.parametrize("skus,expected_amount", [("A"*3, 130), ("A"*6, 260), ("A"*8, 360)])
-def test_checkout_special_offer_A(skus, expected_amount):
+@pytest.mark.parametrize("skus,expected_amount", [("A", 50), ("A"*3, 130), ("A"*6, 260), ("A"*8, 360)])
+def test_checkout_item_A(skus, expected_amount):
     assert checkout(skus) == expected_amount 
 
 
-@pytest.mark.parametrize("skus,expected_amount", [("B"*2, 45), ("B"*4, 90), ("B"*5, 120)])
-def test_checkout_special_offer_A(skus, expected_amount):
+@pytest.mark.parametrize("skus,expected_amount", [("B"*1, 30), ("B"*2, 45), ("B"*4, 90), ("B"*5, 120)])
+def test_checkout_item_A(skus, expected_amount):
+    assert checkout(skus) == expected_amount 
+
+
+@pytest.mark.parametrize("skus,expected_amount", [("C", 20), ("C"*3, 60)])
+def test_checkout_item_C(skus, expected_amount):
     assert checkout(skus) == expected_amount 
 
 
