fe48b4eb6927a56e22e1f87c507724ac77982ad3
sbx_user1051 <sbx_user1051@169.254.9.138>
2024-12-12 17:46:08 +0000

    Thu Dec 12 17:46:08 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,23 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+
+from collections import Counter
+
+PRICE = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+SPECIAL_OFFERS = {
+    "A": {"quantity": 3, "price": 130},
+    "B": {"quantity": 2, "price": 45},
+}
+
+def checkout(skus: str):
+    items = Counter(skus.upper())
+    print(items)
+    
+
+
---
+++ test/solution_tests/CHK/test_one.py
@@ -0,0 +1,6 @@
+from lib.solutions.TST import one
+
+
+class TestSum:
+    def test_sum(self):
+        assert one.get() == 1
