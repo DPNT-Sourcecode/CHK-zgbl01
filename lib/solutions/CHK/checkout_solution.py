755f4358cf41b24e756e1dbfb0d17aeae4c11939
sbx_user1051 <sbx_user1051@169.254.28.250>
2024-12-13 17:40:06 +0000

    Fri Dec 13 17:40:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,16 +12,44 @@ PRICE = {
     "D": 15,
     "E": 40,
     "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
 }
 
 DISCOUNTS = {
     "A": {5: 200, 3: 130},
     "B": {2: 45},
+    "H": {10: 80, 5: 45},
+    "K": {2: 150},
+    "P": {5: 200},
+    "Q": {3: 80},
+    "V": {3: 130, 2: 90},
 }
 
 FREE_ITEMS = {
     "E": {"items_needed": 2, "item": "B"},
     "F": {"items_needed": 2, "item": "F"},
+    "N": {"items_needed": 3, "item": "M"},
+    "R": {"items_needed": 3, "item": "Q"},
+    "U": {"items_needed": 3, "item": "U"},
 }
 
 
@@ -118,5 +146,6 @@ def checkout(skus: str) -> int:
         total_price += calculate_item_price(item, count, free_items)
 
     return total_price
+
 
 
